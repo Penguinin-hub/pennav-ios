cf2694921bfb3c6400926548bfc3e2922956302e
fawzirifai <f.refai@penguinin.com>
2026-03-05 01:58:56 +0300

    1.0.5

--- Package.swift
+++ Package.swift
@@ -19,18 +19,11 @@ let package = Package(
         )
     ],
     targets: [
-        .binaryTarget(name: "_MapboxNavigationHelpers", path: "Frameworks/_MapboxNavigationHelpers.xcframework"),
-        .binaryTarget(name: "_MapboxNavigationLocalization", path: "Frameworks/_MapboxNavigationLocalization.xcframework"),
-        .binaryTarget(name: "libnav_mb", path: "Frameworks/libnav_mb.xcframework"),
         .binaryTarget(name: "MapboxCommon", path: "Frameworks/MapboxCommon.xcframework"),
         .binaryTarget(name: "MapboxCommonWrapper", path: "Frameworks/MapboxCommonWrapper.xcframework"),
         .binaryTarget(name: "MapboxCoreMaps", path: "Frameworks/MapboxCoreMaps.xcframework"),
         .binaryTarget(name: "MapboxCoreMapsWrapper", path: "Frameworks/MapboxCoreMapsWrapper.xcframework"),
-        .binaryTarget(name: "MapboxDirections", path: "Frameworks/MapboxDirections.xcframework"),
         .binaryTarget(name: "MapboxMaps", path: "Frameworks/MapboxMaps.xcframework"),
-        .binaryTarget(name: "MapboxNavigationCore", path: "Frameworks/MapboxNavigationCore.xcframework"),
-        .binaryTarget(name: "MapboxNavigationNative", path: "Frameworks/MapboxNavigationNative.xcframework"),
-        .binaryTarget(name: "MapboxNavigationUIKit", path: "Frameworks/MapboxNavigationUIKit.xcframework"),
         .binaryTarget(name: "pe_logic", path: "Frameworks/pe_logic.xcframework"),
         .binaryTarget(name: "Penguin", path: "Frameworks/Penguin.xcframework"),
         .binaryTarget(name: "PenguinINRenderer", path: "Frameworks/PenguinINRenderer.xcframework"),
@@ -54,13 +47,7 @@ let package = Package(
         .target(
             name: "PenNavRoaming",
             dependencies: [
-                "_MapboxNavigationHelpers",
-                "_MapboxNavigationLocalization",
-                "libnav_mb",
-                "MapboxDirections",
-                "MapboxNavigationCore",
-                "MapboxNavigationNative",
-                "MapboxNavigationUIKit"
+                
             ]
         )
     ]
